5f2e14fedd9b633b168c48e9b7c586368d5d6318
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-24 19:02:42 -0300

    Refactor eventListener request

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,15 @@ async function teste(){
   console.log(await searchUser('pauloeduardodesordigomes@gmail.com'));
   console.log(await searchUser('pauloeduardods'));
 }
-teste();
+//teste();
 const buttonLogin = document.querySelector('.button-login-github');
 
 let userData = {};
 
 buttonLogin.addEventListener('click', async () => {
   const user = document.querySelector('.input-login').value;
-  const fetchGithub = await fetch(`https://api.github.com/users/${user}`);
-  userData = await fetchGithub.json();
+  const userResult = await(searchUser(user));
+  console.log(userResult)
 });
 
 module.exports = { userData };
