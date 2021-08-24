6e90fa9fab27d8a2a50d340bc8627e8f347edc65
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-08-24 18:33:34 -0300

    script da tela incial parte 4

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const buttonLogin = document.querySelector('.button-login-github');
 
 let userData = {};
 
-const getUserData = (user) => {
+const getUserData = async (user) => {
   const fetchGithub = await fetch(`https://api.github.com/users/${user}`);
   const userObject = await fetchGithub.json();
   return userObject;
@@ -11,6 +11,7 @@ const getUserData = (user) => {
 buttonLogin.addEventListener('click', async () => {
   const user = document.querySelector('.input-login').value; // Faz a validação desse dado aqui e depois joga na getUserData
   userData = await getUserData(user);
+  console.log(userData);
 });
 
-module.exports = { userData };
+// module.exports = { userData };
