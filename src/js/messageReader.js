9dccd9f537a1aef3bbd2a61878c3808d1e39fb07
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-08-26 14:38:11 -0300

    Add display message result

--- src/js/messageReader.js
+++ src/js/messageReader.js
@@ -1,20 +1,34 @@
 const messageReader = (string) => {
-	console.log(string)
 	string = string.replace(/[ÀÁÂÃÄÅ]/, "A");
 	string = string.replace(/[àáâãäå]/, "a");
 	string = string.replace(/[ÈÉÊË]/, "E");
 	string = string.replace(/[Ç]/, "C");
 	string = string.replace(/[ç]/, "c");
-	console.log(string)
-	//console.log(String(string).split((/\r?\n/)));
-	//return string.toString().split(/\r?\n/)//.	reduce((acc, cur) => [...acc, cur], []);
-	return string.match(/([^\\\r\n]{1,51}|)[0-9]{2}\h[0-9]{2}[^\\\r\n]{1,8}[0-9]{2}\h{1}[0-9]{2}/g)//[^\\\r\n]{1,}/g); // regex to day message
+	return string.match(/([\[\*\] ]{1,5}|)[0-9]{2}\h[0-9]{2}[^\\\r\n]{1,8}[0-9]{2}\h{1}[0-9]{2}[^\\\r\n]{1,}/g) // regex to day message
 } 
-	
+
+const createCheckbox = (text, time) => {
+	const newCheckbox = document.createElement('div');
+	const newSpanTime = document.createElement('span');
+	const checkboxInput = document.createElement('span');
+	newSpanTime.style.display = 'none';
+	newSpanTime.innerText = time;
+	checkboxInput.innerText = text;
+	newCheckbox.appendChild(newSpanTime);
+	newCheckbox.appendChild(checkboxInput);
+	return newCheckbox;
+}
+
+const displayMessageResult = (result) => {
+	result.forEach((cur) => {
+		document.getElementById('day-message-result').appendChild(createCheckbox(cur, '10h20 as 20h21'));
+	})
+}
+
 
 const sendMessageButton = document.getElementById('day-message-button');
 
 sendMessageButton.addEventListener('click', () => {
 	const message = document.getElementById('day-message').value;
-	console.log(messageReader(message));
+	displayMessageResult(messageReader(message));
 });
